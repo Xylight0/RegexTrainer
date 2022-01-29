a8a7a71079d9a85938141a999e66c12c78106363
Xylight0 <greppn@gmail.com>
2022-01-30 00:02:09 +0100

    button modifier state change

--- client/src/comp/RegexField/Expression/Expression.js
+++ client/src/comp/RegexField/Expression/Expression.js
@@ -4,10 +4,31 @@ import { useRegexSyntax } from "../../../context/Regex_context";
 export default function Expression() {
   const { setRegexSyntax } = useRegexSyntax();
   const [regexInput, setRegexInput] = useState("\\w");
+  const [modifier, setModifier] = useState("");
 
   useEffect(() => {
     setRegexSyntax(regexInput);
   }, [regexInput, setRegexSyntax]);
+
+  function stateChange(e) {
+    if (e.target.value === modifier) {
+      setModifier("");
+    } else {
+      setModifier(e.target.value);
+    }
+
+    if (e.target.classList.contains("bg-gray")) {
+      e.target.classList.remove("bg-gray");
+      e.target.classList.add("bg-custom-blue");
+    } else {
+      e.target.classList.remove("bg-custom-blue");
+      e.target.classList.add("bg-gray");
+    }
+  }
+
+  useEffect(() => {
+    console.log(modifier);
+  }, [modifier]);
 
   return (
     <div className="flex flex-col gap-4">
@@ -23,14 +44,26 @@ export default function Expression() {
             className="border-none text-dark-gray font-bold text-lg w-full"
           />
           <div className="flex flex-row gap-4">
-            <button className="px-2 py-1 rounded-md text-white font-semibold bg-gray">
-              wadad
+            <button
+              onClick={stateChange}
+              value="g"
+              className="btnOne w-8 px-2 py-1 rounded-md text-white font-semibold bg-gray"
+            >
+              G
             </button>
-            <button className="px-2 py-1 rounded-md text-white font-semibold bg-gray">
-              wadad
+            <button
+              onClick={stateChange}
+              value="i"
+              className="btnTwo w-8 px-2 py-1 rounded-md text-white font-semibold bg-gray"
+            >
+              I
             </button>
-            <button className="px-2 py-1 rounded-md text-white font-semibold bg-gray">
-              wadad
+            <button
+              onClick={stateChange}
+              value="m"
+              className="btnThree w-8 px-2 py-1 rounded-md text-white font-semibold bg-gray"
+            >
+              M
             </button>
           </div>
         </div>
