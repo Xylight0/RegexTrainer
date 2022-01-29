b28c5217eddc1fafc795e908166ae2a1e18746b3
Xylight0 <greppn@gmail.com>
2022-01-29 23:07:06 +0100

    buttons for modifier

--- client/src/comp/RegexField/Expression/Expression.js
+++ client/src/comp/RegexField/Expression/Expression.js
@@ -8,7 +8,6 @@ export default function Expression() {
   useEffect(() => {
     setRegexSyntax(regexInput);
   }, [regexInput, setRegexSyntax]);
-  
 
   return (
     <div className="flex flex-col gap-4">
@@ -16,13 +15,24 @@ export default function Expression() {
         <p className="text-dark-gray font-bold font-poppins">Expression</p>
       </div>
       <div className="w-full bg-light-gray h-16 rounded-md flex items-center px-4">
-        <div className="inputBefore font-bold text-lg w-full">
+        <div className="inputBefore font-bold text-lg w-full flex flex-row">
           <input
             type="text"
             defaultValue={regexInput}
             onChange={(e) => setRegexInput(e.target.value)}
             className="border-none text-dark-gray font-bold text-lg w-full"
           />
+          <div className="flex flex-row gap-4">
+            <button className="px-2 py-1 rounded-md text-white font-semibold bg-gray">
+              wadad
+            </button>
+            <button className="px-2 py-1 rounded-md text-white font-semibold bg-gray">
+              wadad
+            </button>
+            <button className="px-2 py-1 rounded-md text-white font-semibold bg-gray">
+              wadad
+            </button>
+          </div>
         </div>
       </div>
     </div>
