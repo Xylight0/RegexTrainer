ca21faa22e0d85009582cf45086a842eee11d014
Xylight0 <greppn@gmail.com>
2022-02-01 17:43:33 +0100

    more information added to json

--- client/src/comp/LearnSection/List.js
+++ client/src/comp/LearnSection/List.js
@@ -1,15 +1,8 @@
 import React from "react";
-import { loremIpsum } from "react-lorem-ipsum";
 let informations = require("./information.json");
 
 export default function List() {
-  let informationList = [];
-
   function addSection() {
-    //console.log(informations.information.section);
-    for (let i = 0; i < 100; i++) {
-      informationList.push("loremIpsum()");
-    }
     return informations.information.section.map((e, i) => (
       <div key={i}>
         <p className="py-4 rounded-md text-black font-bold text-2xl flex w-max grow">
@@ -21,23 +14,35 @@ export default function List() {
   }
 
   function addPoints(index) {
-    //console.log(informations.information.section[index]);
     let pts = [];
     for (var key in informations.information.section[index].points) {
-      console.log(key,informations.information.section[index].points[key]);
       pts.push(key);
     }
     return pts.map((e, i) => (
-      <div key={i} className="bg-gray p-4 rounded-md text-white font-semibold text-lg flex w-max gap-8">
-        <p>
-          {e}
-        </p>
-        <p>
-          {informations.information.section[index].points[e]}
-        </p>
+      <div
+        key={i}
+        className="bg-gray p-4 rounded-md text-white font-semibold text-lg flex w-max gap-8"
+      >
+        <p>{e}</p>
+        <p>{informations.information.section[index].points[e]}</p>
       </div>
     ));
   }
 
-  return <div className="flex flex-col gap-8">{addSection()}</div>;
+  return (
+    <>
+      <div className="pt-2">
+        <p className="text-large font-semibold">
+          JavaScript RegExp Reference by{" "}
+          <a
+            className="text-custom-blue"
+            href="https://www.w3schools.com/jsref/jsref_obj_regexp.asp"
+          >
+            W3S
+          </a>
+        </p>
+      </div>
+      <div className="flex flex-col gap-8">{addSection()}</div>
+    </>
+  );
 }
