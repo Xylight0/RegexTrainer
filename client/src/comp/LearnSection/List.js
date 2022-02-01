928cab01ce601bc4cdb219036dbaff274461193a
Xylight0 <greppn@gmail.com>
2022-02-01 17:29:12 +0100

    points display

--- client/src/comp/LearnSection/List.js
+++ client/src/comp/LearnSection/List.js
@@ -6,30 +6,35 @@ export default function List() {
   let informationList = [];
 
   function addSection() {
-    console.log(informations.information.section);
+    //console.log(informations.information.section);
     for (let i = 0; i < 100; i++) {
       informationList.push("loremIpsum()");
     }
     return informations.information.section.map((e, i) => (
-      <div key={i} className="">
+      <div key={i}>
         <p className="py-4 rounded-md text-black font-bold text-xl flex w-max grow">
           {e.title}
         </p>
-        <div className="flex flex-col gap-4">
-        {addPoints(i)}
-        </div>
+        <div className="flex flex-col gap-4">{addPoints(i)}</div>
       </div>
     ));
   }
 
   function addPoints(index) {
-    console.log(informations.information.section[index]);
-    return (informations.information.section[index].points).map((e, i) => (
-      <div key={i}>
-        <p className="bg-gray p-4 rounded-md text-white flex w-max grow">
-          {e.points}
+    //console.log(informations.information.section[index]);
+    let pts = [];
+    for (var key in informations.information.section[index].points) {
+      console.log(key,informations.information.section[index].points[key]);
+      pts.push(key);
+    }
+    return pts.map((e, i) => (
+      <div key={i} className="bg-gray p-4 rounded-md text-white font-semibold text-lg flex w-max gap-8">
+        <p>
+          {e}
         </p>
-        {}
+        <p>
+          {informations.information.section[index].points[e]}
+        </p>
       </div>
     ));
   }
