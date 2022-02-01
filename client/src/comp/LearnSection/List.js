c91ff1cd2429869c0fd9554e090ff119e9078cce
Xylight0 <greppn@gmail.com>
2022-02-01 17:16:50 +0100

    add points of section

--- client/src/comp/LearnSection/List.js
+++ client/src/comp/LearnSection/List.js
@@ -5,20 +5,34 @@ let informations = require("./information.json");
 export default function List() {
   let informationList = [];
 
-  function addInfo() {
+  function addSection() {
     console.log(informations.information.section);
     for (let i = 0; i < 100; i++) {
       informationList.push("loremIpsum()");
     }
-    //<div key={i} className="bg-gray p-4 rounded-md text-white flex w-max grow">{e.title}</div>
     return informations.information.section.map((e, i) => (
-      <div key={i} className="p-4 rounded-md text-black font-bold text-xl flex w-max grow">{e.title}</div>
-    ))
+      <div key={i} className="">
+        <p className="py-4 rounded-md text-black font-bold text-xl flex w-max grow">
+          {e.title}
+        </p>
+        <div className="flex flex-col gap-4">
+        {addPoints(i)}
+        </div>
+      </div>
+    ));
   }
 
-  return (
-    <div className="flex flex-col gap-2">
-      {addInfo()}
-    </div>
-  );
+  function addPoints(index) {
+    console.log(informations.information.section[index]);
+    return (informations.information.section[index].points).map((e, i) => (
+      <div key={i}>
+        <p className="bg-gray p-4 rounded-md text-white flex w-max grow">
+          {e.points}
+        </p>
+        {}
+      </div>
+    ));
+  }
+
+  return <div className="flex flex-col gap-8">{addSection()}</div>;
 }
