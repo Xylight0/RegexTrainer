137b4d55ed2533ebc7704018343aeb5e961e906d
Xylight0 <greppn@gmail.com>
2022-02-01 16:57:28 +0100

    display json data

--- client/src/comp/LearnSection/List.js
+++ client/src/comp/LearnSection/List.js
@@ -6,12 +6,13 @@ export default function List() {
   let informationList = [];
 
   function addInfo() {
-    console.log(informations);
+    console.log(informations.information.section);
     for (let i = 0; i < 100; i++) {
-      informationList.push(loremIpsum());
+      informationList.push("loremIpsum()");
     }
-    return informationList.map((e, i) => (
-      <div key={i} className="bg-gray p-4 rounded-md text-white flex w-max grow">{e}</div>
+    //<div key={i} className="bg-gray p-4 rounded-md text-white flex w-max grow">{e.title}</div>
+    return informations.information.section.map((e, i) => (
+      <div key={i} className="p-4 rounded-md text-black font-bold text-xl flex w-max grow">{e.title}</div>
     ))
   }
 
